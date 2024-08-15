cf7a16dec1995740737d26957dc0675c63812c71
Jonathan <jondiehl22@gmail.com>
2024-08-15 13:17:11 -0400

    Working on it

--- src/main/java/TreeOneZero.java
+++ src/main/java/TreeOneZero.java
@@ -29,4 +29,71 @@ public class TreeOneZero {
     }
 
     //method to check if a value exists in tree
+    private boolean containsNode(Integer value) {
+        return containsNodeRecursive(root,value);
+    }
+
+    private boolean containsNodeRecursive(Node current, Integer value) {
+        if (current == null) {
+            return false;
+        }
+        
+        if (value.equals(current.value)){
+            return true;
+        }
+        
+        return value < current.value
+                ? containsNodeRecursive(current.left, value)
+                : containsNodeRecursive(current.right, value);
+    }
+    
+    //method to delete a value from tree
+    public void delete(Integer value) { 
+        root = deleteRecursive(root, value);
+    }
+
+    private Node deleteRecursive(Node current, Integer value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value.equals(current.value)) {
+            // Node to delete found
+
+            //first IF: node has no children
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+
+            //second IF: Node has one child
+            if (current.right == null) {
+                return current.left;
+            }
+            if (current.left == null) {
+                return current.right;
+            }
+
+            //third IF: Node has two children
+            Integer smallestValue = findSmallestValue(current.right);
+            current.value = smallestValue;
+            current.right = deleteRecursive(current.right, smallestValue);
+            return current;
+        }
+
+        //recursive search for the node to delete
+        if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+        } else {
+            current.right = deleteRecursive(current.right, value);
+        }
+
+        return current;
+    }
+
+    //method to find smallest value
+    private Integer findSmallestValue(Node right) {
+    }
+
+
+
 }
