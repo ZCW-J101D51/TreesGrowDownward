b1b788ee8b54ab22c8a47d559bd0bfe69c352a46
Jonathan <jondiehl22@gmail.com>
2024-08-15 14:37:19 -0400

    Working in progress

--- src/main/java/TreeOneZero.java
+++ src/main/java/TreeOneZero.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by kristofer on 7/13/20.
  */
@@ -11,7 +14,7 @@ public class TreeOneZero {
 
     //method to add a value to the tree
     public void add(Integer value) {
-        root = addRecursive(root,value);
+        root = addRecursive(root, value);
     }
 
     //RECURSIVE method to add a value
@@ -30,25 +33,25 @@ public class TreeOneZero {
 
     //method to check if a value exists in tree
     private boolean containsNode(Integer value) {
-        return containsNodeRecursive(root,value);
+        return containsNodeRecursive(root, value);
     }
 
     private boolean containsNodeRecursive(Node current, Integer value) {
         if (current == null) {
             return false;
         }
-        
-        if (value.equals(current.value)){
+
+        if (value.equals(current.value)) {
             return true;
         }
-        
+
         return value < current.value
                 ? containsNodeRecursive(current.left, value)
                 : containsNodeRecursive(current.right, value);
     }
-    
+
     //method to delete a value from tree
-    public void delete(Integer value) { 
+    public void delete(Integer value) {
         root = deleteRecursive(root, value);
     }
 
@@ -92,8 +95,76 @@ public class TreeOneZero {
 
     //method to find smallest value
     private Integer findSmallestValue(Node right) {
+        return root.left == null ? root.value : findSmallestValue(root.left);
+
     }
 
+    //method for in - order
+    public void traverseInOrder() {
+        traverseInOrderRecursive(root);
+        System.out.println();
+    }
 
+    //resursive method for in-order traversal
+    private void traverseInOrderRecursive(Node node) {
+        if (node != null) {
+            traverseInOrderRecursive(node.left);
+            System.out.print(" " + node.value);
+            traverseInOrderRecursive(node.right);
+        }
+    }
 
+    //public method for pre-order traversal
+    public void traversePreOrder() {
+        traversePreOrderRecursive(root);
+        System.out.println();
+    }
+
+    //recursive method for pre-order traversal
+    private void traversePreOrderRecursive(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.value);
+            traversePreOrderRecursive(node.left);
+            traversePreOrderRecursive(node.right);
+        }
+    }
+
+    //method for post-order traversal
+    public void traversePostOrder() {
+        traversePostOrderRecursive(root);
+        System.out.println();
+    }
+
+    //recur method for post-order traversal
+    private void traversePostOrderRecursive(Node node) {
+        if (node != null) {
+            traversePostOrderRecursive(node.left);
+            traversePostOrderRecursive(node.right);
+            System.out.print(" " + node.value);
+        }
+    }
+
+    //methodZzzzz for level-order traversal
+    public void traverseLevelOrder() {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> nodes = new LinkedList<>();
+        nodes.add(root);
+
+        while (!nodes.isEmpty()) {
+            Node node = nodes.remove();
+            System.out.print(" " + node.value);
+
+            if (node.left != null) {
+                nodes.add(node.left);
+            }
+
+            if (node.right != null) {
+                nodes.add(node.right);
+            }
+        }
+        System.out.println();
+    }
 }
